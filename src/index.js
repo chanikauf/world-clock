cf850aabcc026c4a5e784d299b2951fd6700f08d
chanikauf <118140788+chanikauf@users.noreply.github.com>
2024-11-12 15:12:55 +0000

    embedded moments.js and replaced HTML with real time

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+function updateTime() {
+  //Johannesburg
+  let johannesburgElement = document.querySelector("#johannesburg");
+  let johannesburgDateElement = document.querySelector("#johannesburg .date");
+  let johannesburgTimeElement = document.querySelector("#johannesburg .time");
 
+  johannesburgDateElement.innerHTML = moment()
+    .tz("Africa/Johannesburg")
+    .format("MMMM Do YYYY");
+  johannesburgTimeElement.innerHTML = moment()
+    .tz("Africa/Johannesburg")
+    .format("HH:mm:ss [<small>]A[</small>]");
+
+  //New York
+
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = document.querySelector("#new-york .date");
+  let newYorkTimeElement = document.querySelector("#new-york .time");
+
+  newYorkDateElement.innerHTML = moment()
+    .tz("America/New_York")
+    .format("MMMM Do YYYY");
+  newYorkTimeElement.innerHTML = moment()
+    .tz("America/New_York")
+    .format("HH:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
